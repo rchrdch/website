54b03719b9bb7505487b20cc596c63af802ba36a
rchrdch <rcho@bu.edu>
2024-02-04 22:41:50 -0500

    Adding initial website

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  plugins: [require("tailwindcss-highlights")],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#1470AF",
+        orange: "#EB8F50",
+        background: "#EAEAEE",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
