3b76ee61b4226614b84139db5a8f3520d92ae414
rchrdch <rcho@bu.edu>
2024-02-05 00:04:43 -0500

    Adding vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   compressHTML: true,
   integrations: [tailwind(), sitemap()],
+
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
