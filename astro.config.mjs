ec9079bf059e9fe97e16c2b5395f28d0512e27d9
rchrdch <rcho@bu.edu>
2024-02-12 02:33:58 -0500

    Adding Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [tailwind(), sitemap()],
-
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: vercel({
     webAnalytics: {
